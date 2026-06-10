a841dcba938b71673dea927c3f514bda14be884c
Seksan Poltree <seksan.poltree@gmail.com>
2026-06-11 01:07:13 +0700

    l10n_th_account_payment_fix: add company_id shim for account.account
    Odoo 18 replaced account.account.company_id (Many2one) with company_ids
    (Many2many). OCA modules such as account_payment_multi_deduction still
    use check_company=True on Many2one fields pointing to account.account,
    which causes web_read to request company_id from account.account and
    raises KeyError: 'company_id' when registering a payment.
    
    Add a computed, non-stored company_id field that returns company_ids[:1]
    so any read of that field succeeds without affecting write paths.

---
+++ l10n_th_account_payment_fix/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Thai Account Payment Fix",
+    "version": "18.0.1.0.0",
+    "author": "Morange Solution Co., Ltd.",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "summary": "Fix KeyError: company_id on account.payment.register in Odoo 18",
+    "depends": ["account"],
+    "auto_install": False,
+    "installable": True,
+}
